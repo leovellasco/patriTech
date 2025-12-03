8614fe9e2b5e2d53a5a4a61d113848b66bdfa77b
leovellasco <leonardo.rjsg@gmail.com>
2025-12-03 19:44:04 -0300

    Setup inicial

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include("core.urls")),
+
+    # Swagger
+    path("openapi/", get_schema_view(
+        title="PATRI-TECH API",
+        description="Documentação da API de inventário de bens",
+        version="1.0.0"
+    ), name="openapi-schema"),
+
+    path("docs/", TemplateView.as_view(
+        template_name="swagger.html"
+    ), name="swagger-ui"),
 ]
